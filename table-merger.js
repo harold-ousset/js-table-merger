e5b3fff2c5656b0cb7e2a437dfd6acb4b2c2a9ed
Harold <harold@ousset.net>
2016-10-09 14:20:22 +0200

    change for array prototype

--- table-merger.js
+++ table-merger.js
@@ -14,149 +14,189 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 */  
-
+  
   /**
-  * Data table merger is used to merge two-dimensional arrays
-  *
-  * @param {Array} Source, Table to modify (2D Array)
-  * @param {Array} Load, Table with fresh data (2D Array)
-  * @param {Array} primaryKeys
-  * @param {Object} options { header : "none" | "source" | "load", normalize : true | false, clear : true | false, skipHeader : true | false}
-  * @return {Object} { result : mergedArray, unmatched : excedentRows }
+  * Table merger is used to merge two-dimensional arrays and come as a prototype of Array Object
+  * @param {Array} Load, Table with data to be added (2D Array)
+  * @param {Array} primaryKeys, header columns used to match 2D arrays
+  * @param {Object} options { header: "none" | "source" | "load", clear: true | false, skipHeader: true | false, appendUnmached: true | false}
+  * @return {Array} the merged Array
+  * option header: allow fusion of table without header if they are from same size and order
+  * option clear: if second array has empty cell, merge will erase content from first array
+  * option skipHeader: remove header of the resulting array
+  * option appendUnmached: add at the end of the resulting array row from second array that do not have correspondance in the first one
   **/
   
-function tableMerger(source, load, pkeys, options) {
-  options = options || {};
-  
-  // Header creation
-  var sourceHeader = [];
-  var loadHeader = [];
-  if (options.header == "none") {
-    sourceHeader = source[0].map(function(cur,idx) {return idx;});
-    loadHeader = sourceHeader.slice();
-  }
-  else if (options.header == "source") {
-    sourceHeader = source.shift();
-    loadHeader = sourceHeader.slice();
-  }
-  else if (options.header == "load") {
-    loadHeader = load.shift();
-    sourceHeader = loadHeader.slice();
-  }
-  else {
-    sourceHeader = source.shift();
-    loadHeader = load.shift();
-  }
-    
-  // normalize load to fit data
-  if (options.normalize == true) {
-    if (["none","source","load"].indexOf(options.header) > -1) {
-      throw "can't normalize without header";
-    }
-    var loadIndex = {};
-    for (var h = 0; h<loadHeader.length; h++) {
-      loadIndex[loadHeader[h]] = h;
-    }
-    var load =load.map( function (row) { return sourceHeader.map(function (head) { return row[loadIndex[head]] || "" } ); });  
-  }
-  else { // check the consistence of the data
-    if (source[0].length !== load[0].length) {
-      throw "unconsistant data: source and load size differ";
-    }
-  }
-  
-  // check data consistency
-  if ( source.length < 1 || load.length < 1) {
-    throw " Empty array can't be handled";
-  }
-  
-  // check for primary key consistence
-  if ("none" != options.header) {
-    pkeys.forEach(
-      function (key) {
-        if (sourceHeader.indexOf(key) == -1) {
-          throw "invalid primary key: "+key;
+(
+  function () {
+    function merge2d(secondMember, pkeys, options) {
+      var source = this.slice();//copy original array
+      var load = secondMember.slice();//copy the second array
+      options = options || {};
+
+      // Header control and creation
+      var sourceHeader = [];
+      var loadHeader = [];
+      if (options.header === 'none') {// return index has header
+        sourceHeader = source[0].map(
+          function (cur, idx) {
+            return idx;
+          }
+        );
+        loadHeader = sourceHeader.slice();//copy source header to load header
+      } else if (options.header === 'source') {
+        sourceHeader = source.shift();
+        loadHeader = sourceHeader.slice();
+      } else if (options.header === 'load') {
+        loadHeader = load.shift();
+        sourceHeader = loadHeader.slice();
+      } else {
+        sourceHeader = source.shift();
+        loadHeader = load.shift();
+      }
+
+      // check data consistency
+      if (source.length < 1 || load.length < 1) {
+        throw 'Empty array can\'t be handled';
+      }
+
+      function buildHeaderIndex(header, idx) {
+        this[header] = idx;
+      }
+
+      // normalize load to fit data
+      if (source.length != load.length) {
+        if (['none', 'source', 'load'].indexOf(options.header) > -1) {
+          // normalization can't happen without correct header
+          throw "can't normalize without header";
         }
-      }
-    );
-  }
-   
-  // build source Index
-  var sourceIndex = {};
-  for (var h = 0; h<sourceHeader.length; h++) {
-    sourceIndex[sourceHeader[h]] = h;
-  }
 
-  /**
-  * function to build sourceObj
-  **/
-  function repObj (obj, arr, line, idx) {
-    if (arr.length>0) {
-      var key = arr.shift();
-      var localKey = line[sourceIndex[key]];
-      obj[localKey] = obj[localKey] || {};
-      obj[localKey] = repObj(obj[localKey], arr, line, idx);
-    }
-    else {
-      if (options.unique == true && obj.index != undefined) {
-        throw "Duplicate primary key identified";
-      }
-      obj.index = obj.index || [];
-      obj.index.push(idx);
-    }
-    return obj;
-  }
+        // build load index
+        var loadIndex = {};// indexed second member array
+        loadHeader.forEach(
+          buildHeaderIndex,
+          loadIndex
+        );
 
-  // build sourceObj
-  var sourceObj = {};
-  source.reduce(
-    function(pre,cur,idx) {
-      return repObj (pre, pkeys.slice(), cur, idx)
-    },
-    sourceObj
-  );
-    
-  // merge
-  var unMatched = load.filter(
-    function (row) {
-      var index = [];
-      pkeys.reduce( // parse each primary key
-        function (pre,key,idx,arr) {
-          if(pre[row[sourceIndex[key]]] == undefined) {
-            return {}; // stop when unmatch
+        function realignRow(head) {
+          return this[loadIndex[head]] || '';
+        }
+
+        function parseLoad(row) {
+          return sourceHeader.map(
+            realignRow,
+            row
+          );
+        }
+
+        // normalisation process
+        load = load.map(
+          parseLoad
+        );
+      }
+
+      // check for primary key consistency
+      // TODO check for doublon in pkeys
+      if (options.header != 'none') {
+        pkeys.forEach(
+          function (key, idx) {
+            if (sourceHeader.indexOf(key) === -1) {
+              throw 'invalid primary key: ' + key;
+            }
           }
-          if(idx == arr.length-1) { // index reached
-            index = pre[row[sourceIndex[key]]].index;
+        );
+      }
+
+      // build source Index
+      var sourceIndex = {};
+      sourceHeader.forEach(
+        buildHeaderIndex,
+        sourceIndex
+      );
+
+      /**
+      * function to build sourceObj
+      **/
+      function repObj(obj, arr, line, idx) {
+        if (arr.length > 0) {
+          var key = arr.shift();
+          var localKey = line[sourceIndex[key]];
+          obj[localKey] = obj[localKey] || {};
+          obj[localKey] = repObj(obj[localKey], arr, line, idx);
+        } else {
+          if (options.unique === true && obj.index !== undefined) {
+            throw 'Duplicate primary key identified (duplicates entries at: ' +
+            idx + ' ) : ' + line;
           }
-          return pre[row[sourceIndex[key]]]; // else return sub object
+
+          obj.index = obj.index || [];
+          obj.index.push(idx);
+        }
+
+        return obj;
+      }
+
+      // build sourceObj
+      var sourceObj = {};
+      source.reduce(
+        function (pre, cur, idx) {
+          return repObj(pre, pkeys.slice(), cur, idx);
         },
+
         sourceObj
       );
-      if (index.length == 0) { // no match
-        return true;
-      }
-      else {
-        for (var ix = 0; ix < index.length; ix++) {
-          if (options.clear == true) { // clear content when load is empty
-            source[index[ix]] = row.slice();
-          }
-          else {
-            source[index[ix]] = row.map( // only aggregator
-              function(val,id) {
-                return val != "" ? val : source[index[ix]][id];
-              }
-            );
-          }
-        }
-      }
-      return false;
-    }
-  );
 
-  // reconnect header
-  if (["none","load"].indexOf(options.header) == -1 && options.skipHeader != true) {
-    source.unshift(sourceHeader);
+      // merge
+      var unMatched = load.filter(
+        function (row) {
+          var index = [];
+          pkeys.reduce(// parse each primary key
+            function (pre, key, idx, arr) {
+              if (pre[row[sourceIndex[key]]] === undefined) {
+                return {}; // stop when unmatch
+              }
+
+              if (idx === arr.length - 1) { // index reached
+                index = pre[row[sourceIndex[key]]].index;
+              }
+
+              return pre[row[sourceIndex[key]]]; // else return sub object
+            },
+
+            sourceObj
+          );
+          if (index.length === 0) { // no match
+            return true;
+          } else {
+            for (var ix = 0; ix < index.length; ix++) {
+              if (options.clear === true) { // clear content when load is empty
+                source[index[ix]] = row.slice();
+              } else {
+                source[index[ix]] = row.map(// only aggregator
+                  function (val, id) {
+                    return val !== '' ? val : source[index[ix]][id];
+                  }
+                );
+              }
+            }
+          }
+
+          return false;
+        }
+      );
+
+      // reconnect header
+      if (['none', 'load'].indexOf(options.header) === -1 && options.skipHeader !== true) {
+        source.unshift(sourceHeader);
+      }
+
+      if (options.appendUnmached === true) {
+        source = source.concat(unMatched);
+      }
+
+      return source;
+    }
+
+    this.merge2d = merge2d;
   }
-  
-  return {result : source, unMatched:unMatched};
-}
+).call(Array.prototype);
